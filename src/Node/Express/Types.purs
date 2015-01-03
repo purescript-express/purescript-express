89b621cfa7c31ed7370f0069dd7912b32260de3f
Nick Obedin <dancingrobot84@gmail.com>
2015-01-03 08:59:11 +0300

    + a couple of http methods

--- src/Node/Express/Types.purs
+++ src/Node/Express/Types.purs
@@ -37,21 +37,29 @@ instance isForeignProtocol :: IsForeign Protocol where
         _ -> Left $ JSONError "Unknown protocol"
 
 
-data Method = ALL | GET | POST | PUT | DELETE
+data Method = ALL | GET | POST | PUT | DELETE | OPTIONS | HEAD | TRACE | CustomMethod String
 
 instance showMethod :: Show Method where
-    show ALL    = "all"
-    show GET    = "get"
-    show POST   = "post"
-    show PUT    = "put"
-    show DELETE = "delete"
+    show ALL     = "all"
+    show GET     = "get"
+    show POST    = "post"
+    show PUT     = "put"
+    show DELETE  = "delete"
+    show OPTIONS = "options"
+    show HEAD    = "head"
+    show TRACE   = "trace"
+    show (CustomMethod method) = method
 
 instance isForeignMethod :: IsForeign Method where
     read value = case readString value of
-        Right "GET"    -> Right GET
-        Right "POST"   -> Right POST
-        Right "PUT"    -> Right PUT
-        Right "DELETE" -> Right DELETE
+        Right "GET"     -> Right GET
+        Right "POST"    -> Right POST
+        Right "PUT"     -> Right PUT
+        Right "DELETE"  -> Right DELETE
+        Right "OPTIONS" -> Right OPTIONS
+        Right "HEAD"    -> Right HEAD
+        Right "TRACE"   -> Right TRACE
+        Right method    -> Right $ CustomMethod method
         _ -> Left $ JSONError "Unknown HTTP method"
 
 type Port = Number
