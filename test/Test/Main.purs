3a0323d752b88e6fb7e101de15ebe8cffa0c44b1
Nikolay Obedin <nikolay.obedin@jetbrains.com>
2015-07-28 14:02:47 +0300

    Move QueryString tests to purescript-test-unit

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -1,61 +1,6 @@
 module Test.Main where
 
 import Prelude
-import Data.Either
-import Data.Maybe
-import Data.Foldable (for_)
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
-import Node.Express.Internal.QueryString
-
-
-type Test a b = { input :: a, output :: b }
-
-doTest :: forall a b e. (Show b, Eq b)
-       => (Test a b -> b) -> Test a b -> Eff ( console :: CONSOLE | e) Unit
-doTest testFn test =
-    let result = testFn test
-        printPassed = log "✔︎"
-        printFailed =
-            log ("✗\n\tExpected: \""
-                  ++ show test.output
-                  ++ "\"\n\tGot: \""
-                  ++ show result
-                  ++ "\"")
-    in if result /= test.output then printFailed else printPassed
-
-queryParserTests =
-    [ {input: "a=b",        output: Right [Param "a" "b"]}
-    , {input: "a=b&b=c",    output: Right [Param "a" "b", Param "b" "c"]}
-    , {input: "a[123]=&b=", output: Right [Param "a[123]" "", Param "b" ""]}
-    , {input: "",           output: Right []}
-    , {input: "a=b%20c",    output: Right [Param "a" "b c"]}
-    , {input: "a=b+c",      output: Right [Param "a" "b c"]}
-    , {input: "a=b=c",      output: Right [Param "a" "b=c"]}
-    , {input: "a%3Db=c",    output: Right [Param "a=b" "c"]}
-    ]
-
--- TODO: find out why this type is not inferring
-queryGetOneTests :: Array (Test (Array Param) (Maybe String))
-queryGetOneTests =
-    [ {input: [Param "b" "c"],                  output: Nothing}
-    , {input: [],                               output: Nothing}
-    , {input: [Param "a" "b"],                  output: Just "b"}
-    , {input: [Param "a" "b", Param "a" "c"],   output: Just "b"}
-    ]
-
-queryGetAllTests =
-    [ {input: [Param "b" "c"],                  output: []}
-    , {input: [],                               output: []}
-    , {input: [Param "a" "b"],                  output: ["b"]}
-    , {input: [Param "a" "b", Param "a" "c"],   output: ["b", "c"]}
-    ]
-
 
 main = do
-    log "Testing parser"
-    for_ queryParserTests $ doTest (\t -> parse t.input)
-    log "Testing getters (one value)"
-    for_ queryGetOneTests $ doTest (\t -> getOne t.input "a")
-    log "Testing getters (all values)"
-    for_ queryGetAllTests $ doTest (\t -> getAll t.input "a")
+    Test.QueryString.runTests
---
+++ test/Test/QueryString.purs
@@ -0,0 +1,49 @@
+module Test.QueryString (runTests) where
+
+import Data.Either
+import Data.Foldable (for_)
+import Data.Maybe
+import Node.Express.Internal.QueryString
+import Prelude
+import Test.Unit
+
+type TestCase a b = { input :: a, expected :: b }
+
+queryParserTestCases =
+    [ {input: "a=b",        expected: Right [Param "a" "b"]}
+    , {input: "a=b&b=c",    expected: Right [Param "a" "b", Param "b" "c"]}
+    , {input: "a[123]=&b=", expected: Right [Param "a[123]" "", Param "b" ""]}
+    , {input: "",           expected: Right []}
+    , {input: "a=b%20c",    expected: Right [Param "a" "b c"]}
+    , {input: "a=b+c",      expected: Right [Param "a" "b c"]}
+    , {input: "a=b=c",      expected: Right [Param "a" "b=c"]}
+    , {input: "a%3Db=c",    expected: Right [Param "a=b" "c"]}
+    ]
+
+queryGetOneTestCases =
+    [ {input: [Param "b" "c"],                  expected: Nothing}
+    , {input: [],                               expected: Nothing}
+    , {input: [Param "a" "b"],                  expected: Just "b"}
+    , {input: [Param "a" "b", Param "a" "c"],   expected: Just "b"}
+    ]
+
+queryGetAllTestCases =
+    [ {input: [Param "b" "c"],                  expected: []}
+    , {input: [],                               expected: []}
+    , {input: [Param "a" "b"],                  expected: ["b"]}
+    , {input: [Param "a" "b", Param "a" "c"],   expected: ["b", "c"]}
+    ]
+
+doTest :: forall a b e. (Show b, Eq b) => (a -> b) -> TestCase a b -> Assertion e
+doTest fun testCase = do
+    let actual = fun testCase.input
+        msg = show actual ++ " should equal " ++ show testCase.expected
+    assert msg (actual == testCase.expected)
+
+runTests = runTest do
+    test "QueryString.parse" do
+        for_ queryParserTestCases $ doTest (\t -> parse t)
+    test "QueryString.getOne" do
+        for_ queryGetOneTestCases $ doTest (\t -> getOne t "a")
+    test "QueryString.getAll" do
+        for_ queryGetAllTestCases $ doTest (\t -> getAll t "a")
