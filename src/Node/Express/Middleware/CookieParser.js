b16ce38dc17d9faa4875b766032bc80ee8fb89f2
Marcin Szamotulski <mszamot@gmail.com>
2017-03-01 11:59:19 +0000

    push changes in CookieParser.js

--- src/Node/Express/Middleware/CookieParser.js
+++ src/Node/Express/Middleware/CookieParser.js
@@ -8,9 +8,11 @@ exports._cookieParser = function(req, res, nxt) {
   }
 }
 
-exports._secretCookieParser = function(req, res, nxt) {
-  return function() {
-    var cp = require("cookie-parser");
-    return cp(true)(req, res, nxt)
+exports._secretCookieParser = function(secret) {
+  return function(req, res, nxt) {
+    return function() {
+      var cp = require("cookie-parser");
+      return cp(secret)(req, res, nxt)
+    }
   }
 }
